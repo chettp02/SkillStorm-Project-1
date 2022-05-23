171aea434187b09f8c67823875ec1a54bc256b3f
chettp02 <104033885+chettp02@users.noreply.github.com>
2022-05-23 12:30:51 -0500

    Add files via upload

---
+++ Migrations/20220510184847_InitialDbCreation.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FlightService.Migrations
+{
+    public partial class InitialDbCreation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Flights",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DepartureDate = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ArrivalDate = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DepartureTime = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ArrivalTime = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DepartureAirport = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ArrivalAirport = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PassengerLimit = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Flights", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Passengers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Profession = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Age = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Passengers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Confirmations",
+                columns: table => new
+                {
+                    Number = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    PassengerId = table.Column<int>(type: "int", nullable: false),
+                    FlightId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Confirmations", x => x.Number);
+                    table.ForeignKey(
+                        name: "FK_Confirmations_Flights_FlightId",
+                        column: x => x.FlightId,
+                        principalTable: "Flights",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Confirmations_Passengers_PassengerId",
+                        column: x => x.PassengerId,
+                        principalTable: "Passengers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Confirmations_FlightId",
+                table: "Confirmations",
+                column: "FlightId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Confirmations_PassengerId",
+                table: "Confirmations",
+                column: "PassengerId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Confirmations");
+
+            migrationBuilder.DropTable(
+                name: "Flights");
+
+            migrationBuilder.DropTable(
+                name: "Passengers");
+        }
+    }
+}
---
+++ Migrations/20220520013056_UpdateConfirmationModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FlightService.Migrations
+{
+    public partial class UpdateConfirmationModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Confirmations",
+                table: "Confirmations");
+
+            migrationBuilder.DropColumn(
+                name: "Number",
+                table: "Confirmations");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ConfirmationId",
+                table: "Confirmations",
+                type: "int",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Confirmations",
+                table: "Confirmations",
+                column: "ConfirmationId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Confirmations",
+                table: "Confirmations");
+
+            migrationBuilder.DropColumn(
+                name: "ConfirmationId",
+                table: "Confirmations");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "Number",
+                table: "Confirmations",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Confirmations",
+                table: "Confirmations",
+                column: "Number");
+        }
+    }
+}
